077254516ce19fbe7f19da95c3bf0d0471178de1
qingbing1 <1348441635@qq.com>
2024-05-10 16:33:42 +0800

    Avoid-bricks
    Avoid-bricks

---
+++ Avoid-bricks/Assets/C#/GameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject block;
+    public float maxX;
+    public Transform spawnPoint;
+    public float spawnRate;
+
+    bool gameStarted = false;
+
+    public TextMeshProUGUI scoreText;
+    int score = 0;
+
+    public GameObject tap;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !gameStarted)
+        {
+            StartSpawning();
+
+            gameStarted = true;
+
+            tap.SetActive(false);
+        }
+        
+    }
+
+    private void StartSpawning()
+    {
+        InvokeRepeating("SpawnBlock", 0.5f, spawnRate);
+    }
+
+    private void SpawnBlock()
+    {
+        Vector3 spawnPos = spawnPoint.position;
+
+        spawnPos.x = Random.Range(-maxX, maxX);
+
+        Instantiate(block, spawnPos, Quaternion.identity);
+
+        score++;
+
+        scoreText.text = score.ToString();
+    }
+
+   
+}
